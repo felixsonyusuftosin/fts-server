3d4f8cd83ced54fc117c76a5ad005bc605f34a74
Felixson Yusuf Tosin <ftosin@botkeeper.com>
2020-05-01 21:01:30 +0100

    dockerise the app

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
-import app from "@src/app"
-import { createHttpTerminator } from "http-terminator"
+import app from '@src/app'
+import { createHttpTerminator } from 'http-terminator'
 
 const NODE_ENV = process.env.NODE_ENV
 
 const port = process.env.PORT
 const host = process.env.HOST
 
-const server = app.listen(port, (error, success) => {
-  if (error) {
+const server = app.listen(port, (error, success)=> {
+  if(error){
     console.error(`could not start the app ${err}`)
-    return
+    return 
   }
   console.log(`FTS server is running at port ${host}:${port}`)
+
 })
 
-if (NODE_ENV === "production") {
-  const httpTerminator = createHttpTerminator({ server })
-  setTimeout(() => {
-    httpTerminator.terminate()
-  }, 1000)
+if (NODE_ENV === 'production'){
+const httpTerminator = createHttpTerminator({ server })
+setTimeout(() => {
+  httpTerminator.terminate()
+}, 1000)
 }
